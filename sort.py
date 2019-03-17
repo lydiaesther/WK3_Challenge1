881dea66325da014de31af0bae7e95803fa19351
lydia <lydiaesther86@gmail.com>
2019-03-17 17:36:36 +0300

    commit sort

--- sort.py
+++ sort.py
@@ -1,6 +1,19 @@
-coffee_list = ["1","2","3","4","5"]
-nums = {'evens':[],'odds':[] 'chars'}
-def list_sort():
+def list():
+  list_sort=[2,0,6,5,1,7,'z','a']
+  nums = {'evens':[],'odds':[],'chars':[]}
+  for z in list_sort:
+    if isinstance(z,str):
+        nums['chars'].append(z)
+    elif isinstance(z,int):
+        if z % 2 !=0:
+             nums['odds'].append(z)
+        elif z % 2 ==0:
+             nums['evens'].append(z)
+  print(nums)
+
+list()
+        
+
         
 
 
